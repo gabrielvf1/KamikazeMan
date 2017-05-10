589316de2f390eea02b91b7e222dfb88d5592709
Vinicius Lima <Vinicius Lima>
2017-05-10 15:12:52 -0300

    A

--- bomber.py
+++ bomber.py
@@ -8,7 +8,15 @@ class bomber:
 		self.screen=screen
 		self.img=pygame.image.load(img)
 
+	def movimentacao(self):
+		pressed=pygame.key.get_pressed()
+		if pressed[pygame.K_w]:self.y-=3
+		elif pressed[pygame.K_s]:self.y+=3
+		elif pressed[pygame.K_d]:self.x+=3
+		elif pressed[pygame.K_a]:self.x-=3
+
+
 	def draw(self):
-		self.screen.blit(self.img,(self.x,self.y))
+		self.screen.blit(pygame.transform.scale(self.img,(70,64)),(self.x,self.y))
 
 
--- teste.py
+++ teste.py
@@ -1,47 +1,12 @@
-import pygame
-
+import pygame 
 pygame.init()
-
-black=(0,0,
-	0)
-pink=(255,0,255)
-red=(255,0,0)
-green=(0,255,0)
-blue=(0,0,255)
-
-is_blue=True
-x=30
-y=30
-color=black
 screen=pygame.display.set_mode((800,600))
-
-pygame.display.set_caption('Yeba')
-
-exit=False
-clock=pygame.time.Clock()
-while not exit:
-	for event in pygame.event.get():
-		if event.type==pygame.QUIT:
-			exit=True
-		if event.type==pygame.KEYDOWN and event.key==pygame.K_SPACE:
-			is_blue=not is_blue	
-
-	pressed=pygame.key.get_pressed()
-	if pressed[pygame.K_UP]:
-		color=pink
-		y-=3
-	if pressed[pygame.K_DOWN]:
-		color=black
-		y+=3
-	if pressed[pygame.K_RIGHT]:
-		color=red
-		x+=3
-	if pressed[pygame.K_LEFT]:
-		color=green
-		x-=3
-	if pressed[pygame.K_SPACE]:
-		color=blue
-	screen.fill((255,255,255))	
-	pygame.draw.circle(screen,color,(x,y),50,0)
-	pygame.display.flip()					
-	clock.tick(60)
+a = pygame.Rect((1, 1), (2, 2))
+b = pygame.Rect((0, 0), (2, 2))
+c = pygame.Rect((0, 0), (1, 1))
+print(a.colliderect(b))
+# 1
+print(a.colliderect(c))
+# 0
+print(b.colliderect(c))
+# 1
